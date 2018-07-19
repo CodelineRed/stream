ecd857fa1c4242609c87ea5fe134c4a69bb0b69c
InsanityMeetsHH <software@insanitymeetshh.net>
2018-07-19 20:15:38 +0200

    Added https rewrite.

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Stream - InsanityMeetsHH</title>
-        <base href="http://stream.insanitymeetshh.net/">
+        <base href="https://stream.insanitymeetshh.net/">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="theme-color" content="#212121">
